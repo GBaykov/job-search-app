e087cb45304cb94e2004a9d45ec4877e76fbd6dc
GBaykov <gleb.baykov99@gmail.com>
2023-05-30 00:29:15 +0300

    feat: add gh-pages link

--- src/utils/localstorage.ts
+++
@@ -1,7 +0,0 @@
-export const setToStorage = (key: string, value: string) => localStorage.setItem(key, value);
-
-export const getFromStorage = (key: string) => localStorage.getItem(key);
-
-export const removeFromStorage = (key: string) => {
-  localStorage.removeItem(key);
-};
